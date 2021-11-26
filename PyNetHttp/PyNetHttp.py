570b021bc94717bde7646c858a0207a3d155ff6a
世元 李 <1498045907@qq.com>
2021-11-26 18:22:01 +0800

    Test

--- PyNetHttp/PyNetHttp.py
+++ PyNetHttp/PyNetHttp.py
@@ -36,6 +36,7 @@ def get_host_ip():
 if __name__ == "__main__":
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     host = get_host_ip()
+    print(host)
     server_socket.bind((host, 8000))
     server_socket.listen(128)
 
