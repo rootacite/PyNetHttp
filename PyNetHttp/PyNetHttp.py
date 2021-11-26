fe7a1476417a3df3e3ea49a0b2e6ce17beb2d7d9
世元 李 <1498045907@qq.com>
2021-11-26 18:06:30 +0800

    Test

--- PyNetHttp/PyNetHttp.py
+++ PyNetHttp/PyNetHttp.py
@@ -1,26 +1,26 @@
 
 # coding:utf-8
 
+#coding=gbk
+
 import socket
 
 from multiprocessing import Process
 
 def handle_client(client_socket):
-    """
-    处理客户端请求
-    """
+   
     request_data = client_socket.recv(1024)
     print("request data:", request_data)
-    # 构造响应数据
+     
     response_start_line = "HTTP/1.1 200 OK\r\n"
     response_headers = "Server: My server\r\n"
     response_body = "<h1>Python HTTP Test</h1>"
     response = response_start_line + response_headers + "\r\n" + response_body
 
-    # 向客户端返回响应数据
+    
     client_socket.send(bytes(response, "utf-8"))
 
-    # 关闭客户端连接
+    
     client_socket.close()
 
 
@@ -31,7 +31,7 @@ if __name__ == "__main__":
 
     while True:
         client_socket, client_address = server_socket.accept()
-        print("[%s, %s]用户连接上了" % client_address)
+        print("[%s, %s]User connect" % client_address)
         handle_client_process = Process(target=handle_client, args=(client_socket,))
         handle_client_process.start()
         client_socket.close()
