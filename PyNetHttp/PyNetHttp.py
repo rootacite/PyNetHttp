f377791a23b6c38c75d2e3ef1b423a071f71ebff
世元 李 <1498045907@qq.com>
2021-11-27 20:52:53 +0800

    Test

--- PyNetHttp/PyNetHttp.py
+++ PyNetHttp/PyNetHttp.py
@@ -26,7 +26,22 @@ def handle_client(client_socket):
         sv+=i;
         sv+="\n"
     print("request data:", sv) 
-     
+
+    try:
+     if request_n[0][0:2]=="POST":
+         print(request_n[len(request_n)-1])
+         tx=request_n[len(request_n)-1]
+         rtx=tx.split(' ')
+         if rtx[0]=='show':
+             print(Data);
+         if rtx[0]=='setq':
+             if len(rtx)<2:
+                 print("Error : SE");
+                 raise;
+             Data['Question']=rtx[1];
+             print(Data)
+    except:
+         pass
     response_start_line = "HTTP/1.1 200 Success\r\n"
     response_headers = "Server: ShiYuan Li\r\n"
     response_body = ""
@@ -50,28 +65,7 @@ def get_host_ip():
     return ip
 
 
-
-def ControlThread():
-    HOST = get_host_ip()
-    PORT = 4468
-
-    us = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
- 
-    us.bind((HOST, PORT))
-    while True:
-        data, address = us.recvfrom(1024)
-
-        tx=str(data,encoding="utf-8")
-        print(address)
-        rtx=tx.split(' ')
-        if rtx[0]=='show':
-            print(Data);
-        if rtx[0]=='setq':
-            if len(rtx)<2:
-                print("Error : SE");
-                continue;
-            Data['Question']=rtx[1];
-            print(Data)
+        
             
 
 if __name__ == "__main__":
@@ -81,9 +75,6 @@ if __name__ == "__main__":
     server_socket.bind((host, 80))
     server_socket.listen(128)
 
-    th = threading.Thread(target=ControlThread);
-    #th.start()
-    #th.join()
     while True:
         client_socket, client_address = server_socket.accept()
         print("[%s, %s]User connect" % client_address)
